80e90aede6210dd9106f3d79083a379225922098
alakhpc <36898190+alakhpc@users.noreply.github.com>
2024-06-01 05:44:57 -0400

    add templates

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   schema: "./app/db/tables/*",
   out: "./drizzle",
   dbCredentials: {
-    url: ".wrangler/state/v3/d1/miniflare-D1DatabaseObject/a250d44dd518b57d147ab64ae3c153c353cacdbee3fecef9eb8eb4b711e9a1c9.sqlite",
+    url: "DATABASE_PATH",
   },
 });
