3860a00c47ab4edddbe661673e79ffba5976d680
alakhpc <36898190+alakhpc@users.noreply.github.com>
2024-06-01 05:52:09 -0400

    Dynamic sqlite pathfinding

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from "drizzle-kit";
+import fs from "node:fs";
+import p from "path";
+
+const WRANGLER_D1_BASE_PATH = ".wrangler/state/v3/d1/miniflare-D1DatabaseObject";
+
+const sqliteFileName = fs
+  .readdirSync(p.join(__dirname, WRANGLER_D1_BASE_PATH))
+  .find((f) => f.endsWith(".sqlite"));
+
+if (!sqliteFileName) {
+  throw new Error("Could not find sqlite file");
+}
 
 export default defineConfig({
   dialect: "sqlite",
   schema: "./app/db/tables/*",
   out: "./drizzle",
   dbCredentials: {
-    url: "DATABASE_PATH",
+    url: p.join(WRANGLER_D1_BASE_PATH, sqliteFileName),
   },
 });
